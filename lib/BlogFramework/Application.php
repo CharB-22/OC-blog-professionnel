e152279c02d9a9f37e6d2b75a3533c9be9e4759a
Charlène Bennevault <c.bennevault@gmail.com>
2021-03-21 16:42:31 +0100

    Update Application.php to link it to the Router

--- lib/BlogFramework/Application.php
+++ lib/BlogFramework/Application.php
@@ -13,6 +13,51 @@ abstract class Application
     $this->httpResponse = new HTTPResponse;
     $this->name = '';
   }
+
+  public function getController()
+  {
+    $router = new Router;
+
+    $xml->load(__DIR__.'/../../App/conf/routes.xml');
+
+    $routes = $xml->getElementsByTagName('route');
+
+        // We go through the xml routes
+        foreach ($routes as $route)
+        {
+          $vars = [];
+    
+          // Check if any variables
+          if ($route->hasAttribute('vars'))
+          {
+            $vars = explode(',', $route->getAttribute('vars'));
+          }
+    
+          // We add the route to the Router
+          $router->addRoute(new Route($route->getAttribute('url'), $route->getAttribute('module'), $route->getAttribute('action'), $vars));
+        }
+    
+        try
+        {
+          // We get the route matching the URL
+          $matchedRoute = $router->getRoute($this->httpRequest->requestURI());
+        }
+        catch (\RuntimeException $e)
+        {
+          if ($e->getCode() == Router::NO_ROUTE)
+          {
+            // If there is no route matching the URL we redirect to an error page
+            $this->httpResponse->redirect404();
+          }
+        }
+    
+        // We had the vars to the array $_GET.
+        $_GET = array_merge($_GET, $matchedRoute->vars());
+    
+        // On instancie le contrôleur.
+        $controllerClass = 'App\\'.$this->name.'\\Modules\\'.$matchedRoute->module().'\\'.$matchedRoute->module().'Controller';
+        return new $controllerClass($this, $matchedRoute->module(), $matchedRoute->action());
+  }
   
   abstract public function run();
   
