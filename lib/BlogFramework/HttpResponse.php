e6e79d95b63602a9dbcfafae3b433252ab335cbc
Charlène Bennevault <c.bennevault@gmail.com>
2021-03-21 18:01:24 +0100

    Update HttpResponse.php for error management

--- lib/BlogFramework/HttpResponse.php
+++ lib/BlogFramework/HttpResponse.php
@@ -19,7 +19,12 @@ class HttpResponse extends ApplicationComponent
 
     public function redirect404()
     {
-        // will add code later
+        $this->page = new Page($this->app);
+        $this->page->setContentFile(__DIR__.'/../../Errors/404.html');
+        
+        $this->addHeader('HTTP/1.0 404 Not Found');
+        
+        $this->send();
     }
 
     public function send()
