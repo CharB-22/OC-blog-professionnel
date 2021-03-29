aaab7f230b3d33a2f65a3c7ec8bcae6ebc69e5db
Charlène Bennevault <c.bennevault@gmail.com>
2021-03-29 15:01:17 +0200

    Create the hydrate() method in Post.php

--- App/Model/Post.php
+++ App/Model/Post.php
@@ -9,6 +9,19 @@ class Post
     protected $dateModification;
     protected $authorId;
 
+    public function hydrate(array $postData)
+    {
+        foreach ($postData as $key => $value)
+        {
+          $method = 'set'.ucfirst($key);
+              
+          if (method_exists($this, $method))
+          {
+            $this->$method($value);
+          }
+        }    
+    }
+
     public function getId()
     {
         return $this->id;
