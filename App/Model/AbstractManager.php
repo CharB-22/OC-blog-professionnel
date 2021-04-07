f38f3303aa15a7500539ee89bdd63bfe0851d98a
Charlène Bennevault <c.bennevault@gmail.com>
2021-04-07 15:41:22 +0200

    Refactor : remove logic from Router + follow naming logic

--- App/Model/AbstractManager.php
+++ App/Model/AbstractManager.php
@@ -1,6 +1,6 @@
 <?php
 
-abstract class Manager
+abstract class AbstractManager
 {
 
   protected $db;
--- config/Router.php
+++ config/Router.php
@@ -16,41 +16,50 @@
         {
             try
             {   
-                $route = $_GET['route'];
-                
-                switch($route)
+                if (isset($_GET['route']))
                 {
-                    case 'home':
-                        $this->frontendController->getHome();
-                        break;
-                    case 'bloglist':
-                        $this->frontendController->getBlogList();
-                        break;
-                    case 'post':
-                        $this->frontendController->getPost($_GET['id']);
-                        break;
-                    case 'adminHome':
-                        $this->backendController->getAdminHome();
-                        break;
-                    case 'adminPostList':
-                        $this->backendController->getAdminPostList();
-                        break;
-                    case 'adminCommentList':
-                        $this->backendController->getCommentsToApprove();
-                        break;
-                    case 'adminCreatePost':
-                        $this->backendController->getAdminCreatePost();
-                        break;
-                    case 'adminUpdatePost':
-                        $this->backendController->getAdminUpdatePost($_GET['id']);
-                        break;
-                    case 'adminDeletePost':
-                        $this->backendController->getAdminDeletePost($_GET['id']);
-                        break;
-                    default:
-                        $this->frontendController->getHome();
-                        break;
+                    $route = $_GET['route'];
+                
+                    switch($route)
+                    {
+                        case 'home':
+                            $this->frontendController->getHome();
+                            break;
+                        case 'bloglist':
+                            $this->frontendController->getBlogList();
+                            break;
+                        case 'post':
+                            $this->frontendController->getPost();
+                            break;
+                        case 'adminHome':
+                            $this->backendController->getAdminHome();
+                            break;
+                        case 'adminPostList':
+                            $this->backendController->getAdminPostList();
+                            break;
+                        case 'adminCommentList':
+                            $this->backendController->getCommentsToManage();
+                            break;
+                        case 'adminCreatePost':
+                            $this->backendController->getAdminCreatePost();
+                            break;
+                        case 'adminUpdatePost':
+                            $this->backendController->getAdminUpdatePost();
+                            break;
+                        case 'adminDeletePost':
+                            $this->backendController->getAdminDeletePost();
+                            break;
+    
+                        default:
+                            echo "Cette page n'existe pas";
+                            break;
+                    }
                 }
+                else
+                {
+                    $this->frontendController->getHome();
+                }
+
             }
             catch(Exception $e)
             {
