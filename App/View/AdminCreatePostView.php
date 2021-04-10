954444e9e5a2dc14e47f7ec8a43d1163f5cc6a19
Charlène Bennevault <c.bennevault@gmail.com>
2021-04-10 15:52:03 +0200

    Add the customer message logic to the CreatePost view

--- App/View/AdminCreatePostView.php
+++ App/View/AdminCreatePostView.php
@@ -1,20 +1,28 @@
 <?php $this->title = "Créer un article"; ?>
 
 <h1 class="text-center mb-3">Créer un nouvel article</h1>
-
+<?php 
+    if (!empty($message))
+    {
+        echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                $message
+                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+             </div>";
+    }
+?>
 <div>
     <form method="post" action="index.php?route=adminCreatePost" class="p-md-5 p-xs-3">
         <div class="row m-3">
             <label for="title" class="form-label">Titre</label>
-            <input type="text" class="form-control" id="title" name="title" aria-describedby="postTitle">
+            <input type="text" class="form-control" id="title" name="title" value="<?= $newPost->getTitle()!=null? htmlspecialchars($newPost->getTitle()) : null ?>" aria-describedby="postTitle">
         </div>
         <div class="row m-3">
             <label for="excerpt" class="form-label">Introduction</label>
-            <textarea class="form-control" id="excerpt" name="excerpt" aria-describedby="postExcerpt"></textarea>
+            <textarea class="form-control" id="excerpt" name="excerpt" aria-describedby="postExcerpt"><?= $newPost->getExcerpt()!=null? htmlspecialchars($newPost->getExcerpt()) : null ?></textarea>
         </div>
         <div class="row m-3">
             <label for="content" class="form-label">Contenu</label>
-            <textarea class="form-control" id="content" name="content" aria-describedby="Content"></textarea>
+            <textarea class="form-control" id="content" name="content" aria-describedby="Content"><?= $newPost->getContent()!=null? htmlspecialchars($newPost->getContent()) : null ?></textarea>
         </div>
         <button type="submit" name="createPost" class="btn btn-primary m-3">Créer</button>
     </form>
